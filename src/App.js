5ffb2d977fda2fb4fc575d9efe8284b865e75c80
Ivanna Kvasha <ivanna9377@gmail.com>
2023-03-23 17:53:02 -1200

    Redux - final project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Dishes from './Components/DishesComponents/Dishes';
+import AllCategories from './Components/Filter/AllCategories';
+import Cart from './Components/Cart/Cart';
+
+function App() {
+  return (
+  <div className="App">
+  <div className='block'>
+  <AllCategories/>
+  <Cart/>
+  </div>
+  <div className='block'>
+  <Dishes/>
+  </div>
+  </div>
+  );
+}
+
+export default App;
